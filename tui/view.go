c3e35f69e9fdb1b10a9e24d31d223b5e64799623
Ladislav Prskavec <ladislav@prskavec.net>
2024-04-04 08:24:36 +0200

    Add basic markdown render

--- tui/view.go
+++ tui/view.go
@@ -1,12 +1,22 @@
 package tui
 
+import (
+	"log"
+
+	"github.com/charmbracelet/glamour"
+)
+
 func (m model) View() string {
 	var s string
 	if len(m.choice) > 0 {
 		s += m.choice
 		s += "\n\n"
 		s += m.content
-		return s
+		out, err := glamour.Render(s, "dark")
+		if err != nil {
+			log.Println(err)
+		}
+		return out
 	}
 	return m.list.View()
 }
