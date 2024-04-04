9111e5646c214e9bdd8b9788d3e89f24d0178a91
Ladislav Prskavec <ladislav@prskavec.net>
2024-04-04 07:38:18 +0200

    Add list component

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/abtris/rss-bubbletea/tui"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/mmcdole/gofeed"
 )
 
@@ -11,9 +14,27 @@ func main() {
 	file, _ := os.Open("data/podcast.xml")
 	defer file.Close()
 	fp := gofeed.NewParser()
-	feed, _ := fp.Parse(file)
-	fmt.Println(feed.Title)
-	for _, episode := range feed.Items {
-		fmt.Println(episode.Title)
+	feed, err := fp.Parse(file)
+	if err != nil {
+		log.Fatal("parse feed failed", err)
+	}
+
+	model, err := tui.NewModel(feed)
+	if err != nil {
+		log.Fatal("create model failed", err)
+	}
+
+	if len(os.Getenv("DEBUG")) > 0 {
+		f, err := tea.LogToFile("debug.log", "debug")
+		if err != nil {
+			fmt.Println("fatal:", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
+
+	p := tea.NewProgram(model, tea.WithAltScreen(), tea.WithMouseCellMotion())
+	if err := p.Start(); err != nil {
+		log.Fatal("start failed: ", err)
 	}
 }
---
+++ tui/update.go
@@ -0,0 +1,35 @@
+package tui
+
+import (
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.list.SetWidth(msg.Width)
+		return m, nil
+
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "q", "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+
+		case "enter":
+			log.Println("Selected item")
+			i, ok := m.list.SelectedItem().(item)
+			if ok {
+				m.choice = string(i.title)
+				log.Printf("Selected item: %v", m.choice)
+			}
+			return m, tea.Quit
+		}
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
